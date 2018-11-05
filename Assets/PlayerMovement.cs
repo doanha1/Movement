d7543ca231cc2100e061ae2b0e5c4baf0efdc94b
Doan Ha <trong.ha@elev.ga.lbs.se>
2018-11-05 10:51:23 +0100

    Movement

---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [Range(0,20f)]
+    public float moveSpeed;
+    public float jumpHeight;
+
+    private Rigidbody2D rbody;
+
+    public GroundCheck groundCheck;
+    void Start()
+    {
+        rbody = GetComponent<Rigidbody2D>();
+    }
+
+
+    void Update()
+    {
+        rbody.velocity = new Vector2(Input.GetAxisRaw("Horizontal") * moveSpeed, rbody.velocity.y);
+
+        if (Input.GetKeyDown(KeyCode.Space) && groundCheck.touches > 0)
+        {
+            rbody.velocity = new Vector2(rbody.velocity.x, jumpHeight);
+        }
+    }
+}
