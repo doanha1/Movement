11a246c284a083e8343659bd8aa3a613182b23fb
Doan Ha <trong.ha@elev.ga.lbs.se>
2018-11-08 11:03:23 +0100

    Movement

---
+++ Assets/Victory.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Victory : MonoBehaviour
+{
+    public string levelToLoad = "SampleScene";
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Player")
+        {
+            print("GG EZ");
+            SceneManager.LoadScene(levelToLoad);
+        }
+    }
+
+}
